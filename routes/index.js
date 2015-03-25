00effeab5be0dbf1cd32b4a1f1250fc38db26705
Andrew Skaggs <askaggs@kabbage.com>
2015-03-25 18:39:43 -0400

    Angular up in this bitch

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index.html', { title: 'Lunch Translator' });
+  res.render('index.html');
 });
 
 module.exports = router;
