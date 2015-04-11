032227c54c7f431a98abb727906ac3ea31193cf8
Andrew Skaggs <askaggs@kabbage.com>
2015-04-11 13:49:55 -0400

    added cleaning to gulp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,12 +1,17 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var del = require('del');
 
 var paths = {
     buildDir: 'build/',
     distDir:  '../server/client/'
 };
 
-gulp.task('default', ['deploy', 'browser-sync', 'watch']);
+gulp.task('default', ['clean', 'deploy', 'browser-sync', 'watch']);
+
+gulp.task('clean', function() {
+    del.sync([paths.buildDir, paths.distDir], { force: true });
+});
 
 gulp.task('deploy', function() {
     gulp.src('app/**')
