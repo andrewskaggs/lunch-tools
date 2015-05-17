2762dd85a36b8c26af746a45a27efc9e768d5b23
Andrew Skaggs <andrew@andrewskaggs.com>
2015-05-17 11:56:41 -0400

    routing fixes

--- client/app/app.js
+++ client/app/app.js
@@ -8,11 +8,12 @@ var lunchApp = angular.module('lunchApp', [
 lunchApp.config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider
-      .when('', { redirectTo: '/translations' })
+      .when('/', { redirectTo: '/translations' })
       .when('/menus', {templateUrl: 'views/menus.html', controller: 'menusController'})
       .when('/showme', {templateUrl: 'views/showme.html', controller: 'showMeController'})
       .when('/statistics', {templateUrl: 'views/statistics.html', controller: 'statisticsController'})
-      .when('/translations', {templateUrl: 'views/translations.html', controller: 'translationsController'});
+      .when('/translations', {templateUrl: 'views/translations.html', controller: 'translationsController'})
+      .otherwise( { redirectTo: '/' });
 
     $locationProvider.html5Mode(false).hashPrefix('!');
   }
