344d11210ef07b2cae0d86cc976834b509b88d2d
Andrew Skaggs <andrew@andrewskaggs.com>
2015-05-16 23:32:31 -0400

    menus page does stuff

--- client/app/controllers/menus.js
+++ client/app/controllers/menus.js
@@ -2,8 +2,17 @@
 
 var controllers = angular.module('lunchControllers');
 
-controllers.controller('menusController', [ '$scope',
-  function($scope) {
-    $scope.message = "menu stuff";
+controllers.controller('menusController', [ '$scope', '$http',
+  function($scope, $http) {
+
+    $scope.refresh = function() {
+      $http.get('lunches').success(
+        function(data, status, headers, config) {
+          $scope.menus = data;
+        });
+    };
+
+    $scope.refresh();
   }
+
 ]);
