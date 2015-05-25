b6050a1d8f9b31fa0c04c147d286d25b1c34af4e
Andrew Skaggs <askaggs@kabbage.com>
2015-05-24 23:53:35 -0400

    outlined showme page

--- client/app/controllers/showme.js
+++ client/app/controllers/showme.js
@@ -4,6 +4,33 @@ var controllers = angular.module('lunchControllers');
 
 controllers.controller('showMeController', [ '$scope',
   function($scope) {
-    $scope.message = "show me coming soon";
+
+    $scope.getLunch = function(date) {
+      // TODO: call the API /lunches/date/YYYY-MM-DD/translate
+      $scope.lunch = {
+        date: date,
+        menu: 'Churros',
+        translation: 'Motherfuckin Churros!'
+      }
+
+      // TODO: call google images
+      $scope.lunch.image = 'http://www.mrchurrosusa.com/images/Churros_CaramelSplenda_big.jpg';
+
+    };
+
+    $scope.nextDay = function() {
+      // TODO: add a day to $scope.today and the call getLunch
+      console.log('next');
+    };
+
+    $scope.previousDay = function() {
+      // TODO: subtract a day from $scope.today and the call getLunch
+      console.log('previous');
+    };
+
+    // TODO: add moment to bower and angular and use it to get the real date
+    $scope.today = '2015-05-26';
+    $scope.getLunch($scope.today);
+
   }
 ]);
