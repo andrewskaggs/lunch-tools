95a66df273a085ac05753f32277765e1bfba0e13
Andrew Skaggs <andrew@andrewskaggs.com>
2015-05-31 01:32:57 -0400

    Added SASS to implement some media queries for showme improvements

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,29 +1,33 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var del = require('del');
-bower = require('gulp-bower');
+var gulp = require('gulp'),
+  browserSync = require('browser-sync'),
+  del = require('del'),
+  bower = require('gulp-bower'),
+  sass = require('gulp-sass');
 
 var paths = {
-    appDir: 'app/**',
-    buildDir: 'build/',
-    distDir:  '../server/client/',
-    bowerDir: 'bower_components/'
+    app: 'app/**',
+    build: 'build/',
+    dist: '../server/client/',
+    bower: 'bower_components/',
+    sass: 'app/sass/',
+    css: 'css',
+    bootstrap: 'bower_components/bootstrap-sass/assets/stylesheets'
 };
 
-gulp.task('default', ['clean', 'bower', 'deploy', 'browser-sync', 'watch']);
+gulp.task('default', ['clean', 'bower', 'sass', 'deploy', 'browser-sync', 'watch']);
 
 gulp.task('bower', function() { 
-    return bower().pipe(gulp.dest(paths.distDir + paths.bowerDir)) ;
+    return bower().pipe(gulp.dest(paths.dist + paths.bower)) ;
 });
 
 gulp.task('clean', function() {
-    del.sync([paths.buildDir, paths.distDir], { force: true });
+    del.sync([paths.build, paths.dist], { force: true });
 });
 
 gulp.task('deploy', function() {
-    gulp.src(paths.appDir)
+    gulp.src(paths.app)
     // Perform minification tasks, etc here
-    .pipe(gulp.dest(paths.distDir));
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('reload', function() {
@@ -38,6 +42,16 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.bowerDir, ['bower', 'reload']);
-    gulp.watch(paths.appDir, ['deploy', 'reload']);
+    gulp.watch(paths.app, ['bower', 'sass', 'deploy', 'reload']);
+});
+
+gulp.task('sass', function() {
+    gulp.src('app/sass/**/*.scss')
+      .pipe(sass({
+            includePaths: [
+                './app/sass',
+                './bower_components/bootstrap-sass/assets/stylesheets'
+             ]
+         }))
+      .pipe(gulp.dest(paths.dist + paths.css));
 });
