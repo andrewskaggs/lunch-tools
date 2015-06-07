3efba71a6c5b71d6264cadecded861d345d88baf
Andrew Skaggs <andrew@andrewskaggs.com>
2015-06-07 00:42:30 -0400

    fa-spin easter egg

---
+++ client/app/directives/spin.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var directives = angular.module('lunchDirectives');
+
+directives.directive('lunchSpin',
+  function($location) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        element.addClass('lunch-spin');
+        element.hover(function() {
+          angular.element('.lunch-spin').toggleClass('fa-spin');
+        });
+      }
+    }
+  }
+);
